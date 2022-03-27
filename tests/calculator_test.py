0a045e6e41fede5fcdabe0322368f3f75fba9126
pm42 <pm42@njit.edu>
2022-03-27 18:09:34 -0400

    Included test case for division

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -9,21 +9,40 @@ def tuple_list():
 
 
 def test_calculator_add_method():
-    """Testing the Calculator"""
-    # this is show using the calculator object add method
+    """Testing the Calculator Addition"""
 
-    ## Act for AAA testing
+    # Act for AAA testing
     result = Calculator.add(tuple_list())
 
-    ## Assertion for AAA testing
+    # Assertion for AAA testing
     assert result == 3
 
 
 def test_calculator_subtract_method():
-    """Testing the Calculator Subtract"""
-    assert Calculator.subtract(tuple_list()) == -1
+    """Testing the Calculator Subtraction"""
+
+    # Act for AAA testing
+    result = Calculator.subtract(tuple_list())
+
+    # Assertion for AAA testing
+    assert result == -1
 
 
 def test_calculator_multiply_method():
-    """Testing the Calculator Subtract"""
-    assert Calculator.multiply(tuple_list()) == 2
+    """Testing the Calculator Multiplication"""
+
+    # Act for AAA testing
+    result = Calculator.multiply(tuple_list())
+
+    # Assertion for AAA testing
+    assert result == 2
+
+
+def test_calculator_divide_method():
+    """Testing the Calculator Multiplication"""
+
+    # Act for AAA testing
+    result = Calculator.divide(tuple_list())
+    print(result)
+    # Assertion for AAA testing
+    assert result == 0.5
--- tests/operations_test.py
+++ tests/operations_test.py
@@ -1,18 +1,23 @@
 """Testing the calculator operations """
 
-from calculator.operations import Addition, Subtraction, Multiplication
+from calculator.operations import Addition, Subtraction, Multiplication, Division
 
 
 def test_calculator_operations_add():
-    """Testing the Calculator"""
-    assert Addition.add(1, 1) == 2
+    """Testing the Calculator addition"""
+    assert Addition.add(2, 3) == 5
 
 
 def test_calculator_operations_subtract():
-    """Testing the Calculator"""
-    assert Subtraction.subtract(1, 1) == 0
+    """Testing the Calculator subtraction"""
+    assert Subtraction.subtract(1, 3) == -2
 
 
 def test_calculator_operations_multiply():
-    """Testing the Calculator"""
-    assert Multiplication.multiply(1, 1) == 1
+    """Testing the Calculator multiplication"""
+    assert Multiplication.multiply(3, 2) == 6
+
+
+def test_calculator_operations_divide():
+    """Testing the Calculator division"""
+    assert Division.divide(3, 2) == 1.5
